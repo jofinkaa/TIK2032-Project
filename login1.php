2ea8de8666bedde97d9a91cad8768d3d4b7c34ad
jofinkaa <162988829+jofinkaa@users.noreply.github.com>
2024-05-19 13:09:44 +0800

    Create login1.php

---
+++ login1.php
@@ -0,0 +1,23 @@
+<?php
+require 'connect.php'; 
+
+$username = $_POST["user"];
+$password = $_POST["pass"];
+$email = $_POST["email"];
+
+
+$query_sql = "INSERT INTO login (username, password, email)
+              VALUES ('$username', '$password', '$email')";
+
+
+if (mysqli_query($conn, $query_sql)) {
+    
+    header("Location: index.html");
+} else {
+   
+    echo "Pendaftaran gagal: " . mysqli_error($conn);
+}
+
+
+mysqli_close($conn);
+?>
